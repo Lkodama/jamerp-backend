51b9c80f201b84f5c8e8d0ee99831444b3fe79ef
leokodama <leomkodama@gmail.com>
2017-06-20 00:22:44 -0300

    Add CORS configuration

--- jamerp-web/src/main/java/com/br/kodamalabs/configuration/WebConfiguration.java
+++ jamerp-web/src/main/java/com/br/kodamalabs/configuration/WebConfiguration.java
@@ -2,8 +2,12 @@ package com.br.kodamalabs.configuration;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableAutoConfiguration
@@ -11,4 +15,14 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableMongoRepositories(basePackages = "com.br.kodamalabs.repository")
 public class WebConfiguration {
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/produtos").allowedOrigins("*");
+                
+            }
+        };
+    }
 }
